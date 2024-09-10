333762c0016d9335bc2ddd7fe0dc705fb7687d81
Joel-HackMaster <122944694+Joel-HackMaster@users.noreply.github.com>
2024-09-10 11:40:44 -0500

    prueba ante error

--- src/main/java/com/proyecto_biblioteca/proyectobiblioteca/dto/UsuarioDTO.java
+++ src/main/java/com/proyecto_biblioteca/proyectobiblioteca/dto/UsuarioDTO.java
@@ -1,4 +1,49 @@
 package com.proyecto_biblioteca.proyectobiblioteca.dto;
 
 public class UsuarioDTO {
+    private String idUsuario;
+    private String nombres;
+    private String apellidos;
+    private String email;
+    private String dni;
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
 }
--- src/main/java/com/proyecto_biblioteca/proyectobiblioteca/repository/UsuarioRepository.java
+++ src/main/java/com/proyecto_biblioteca/proyectobiblioteca/repository/UsuarioRepository.java
@@ -1,4 +1,14 @@
 package com.proyecto_biblioteca.proyectobiblioteca.repository;
 
-public interface usuarioRepository {
+import com.proyecto_biblioteca.proyectobiblioteca.entity.UsuarioEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
+    @Query(value = "SELECT * FROM obtenerAlumnos()", nativeQuery = true)
+    List<Object[]> obtenerAlumnos();
 }
