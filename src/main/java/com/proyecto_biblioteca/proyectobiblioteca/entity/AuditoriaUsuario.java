5065f68a328222b2c871eb4d511cd5fe485c15ad
Joel-HackMaster <u21216300@utp.edu.pe>
2024-09-18 23:45:45 -0500

    agregado Entidades

--- src/main/java/com/proyecto_biblioteca/proyectobiblioteca/entity/AuditoriaUsuario.java
+++ src/main/java/com/proyecto_biblioteca/proyectobiblioteca/entity/AuditoriaUsuario.java
@@ -1,4 +1,46 @@
 package com.proyecto_biblioteca.proyectobiblioteca.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "tt_auditoriausuario", schema = "public")
 public class AuditoriaUsuario {
+    @Id
+    @Column(name = "id")
+    private Integer idAuditoria;
+
+    @Column(name = "id_usuario")
+    private String idUsuario;
+
+    @Column(name = "id_bibliotecario")
+    private String idBibliotecario;
+
+    @Column(name = "id_accion")
+    private Integer idAccion;
+
+    @Column(name = "detalle_au")
+    private String detalleAu;
+
+    @Column(name = "fecha_au")
+    private LocalDate fechaAu;
+
+    @Column(name = "hora_au")
+    private LocalTime horaAu;
+
+    @Column(name = "ip_con")
+    private String ipCon;
+
+    @Column(name = "nav_con")
+    private String navCon;
+
+    @Column(name = "disp_con")
+    private String dispCon;
+
+    @Column(name = "os_con")
+    private String osCon;
 }
