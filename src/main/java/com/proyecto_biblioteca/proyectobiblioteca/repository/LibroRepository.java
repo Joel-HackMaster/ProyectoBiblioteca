5469165c64e036ef1eb28ac6bd23a9be693c7189
bookworm <bookworm@aleeas.com>
2024-10-23 10:21:53 -0500

    ILibroRepository created

--- src/main/java/com/proyecto_biblioteca/proyectobiblioteca/repository/LibroRepository.java
+++ src/main/java/com/proyecto_biblioteca/proyectobiblioteca/repository/LibroRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class LibroRepository implements IAuthRepository<LibroEntity, String> {
+public class LibroRepository implements IGenericRepository<LibroEntity, String> {
 
     @Autowired
     private UsuarioJpaRepository usuarioJpaRepository;
