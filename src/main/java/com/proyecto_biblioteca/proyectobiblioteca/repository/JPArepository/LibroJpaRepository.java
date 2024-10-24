891cc8ce3cfafa85f75d7dfeaef11c824c12c254
nebula <nebula@aleeas.com>
2024-10-24 18:31:16 -0500

    updated files

--- src/main/java/com/proyecto_biblioteca/proyectobiblioteca/repository/JPArepository/LibroJpaRepository.java
+++ src/main/java/com/proyecto_biblioteca/proyectobiblioteca/repository/JPArepository/LibroJpaRepository.java
@@ -10,15 +10,4 @@ import java.util.List;
 import java.util.Optional;
 
 public interface LibroJpaRepository extends JpaRepository<LibroEntity, Long> {
-    @Query(value = "SELECT * FROM obtenerlibros(:rol)", nativeQuery = true)
-    List<Object[]> obtenerLibros(@Param("rol") Integer rol);
-
-    @Transactional
-    @Query(value = "SELECT * FROM agregarlibro(:tipo, :nombre, :genero)", nativeQuery = true)
-    Object crearUsuario(@Param("tipo") Integer tipo, @Param("nombre") String nombre, @Param("genero") String genero);
-
-    @Query(value = "SELECT * FROM obtenerlibroid(:id_libro)", nativeQuery = true)
-    Optional<LibroEntity> obtenerLibroID(@Param("id_libro") String id_libro);
-
-    Optional<LibroEntity> findByNombreUs(String nombre);
 }
