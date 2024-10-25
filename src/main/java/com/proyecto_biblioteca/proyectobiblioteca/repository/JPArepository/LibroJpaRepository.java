7c714dfbb211bbddf2622426cb79e59b17e3cd50
nebula <nebula@aleeas.com>
2024-10-24 20:00:15 -0500

    updated files

--- src/main/java/com/proyecto_biblioteca/proyectobiblioteca/repository/JPArepository/LibroJpaRepository.java
+++ src/main/java/com/proyecto_biblioteca/proyectobiblioteca/repository/JPArepository/LibroJpaRepository.java
@@ -1,13 +1,7 @@
 package com.proyecto_biblioteca.proyectobiblioteca.repository.JPArepository;
 
 import com.proyecto_biblioteca.proyectobiblioteca.entity.LibroEntity;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface LibroJpaRepository extends JpaRepository<LibroEntity, Long> {
 }
